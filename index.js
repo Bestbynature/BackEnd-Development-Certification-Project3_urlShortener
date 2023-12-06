b4bb410b17e1bef58e4b44d1021ae23caa292394
Dammy <bestbynature2010@gmail.com>
2023-12-06 09:25:48 +0100

    Add initial solution

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+// Initialize an array to store URL objects
+const urlDatabase = [];
+let id = 1;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Serve static files from the 'public' directory
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// Endpoint to handle URL shortening
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+
+  // Check if the URL is valid
+  const urlPattern = /^(https?:\/\/)?(www\.)?[\w.-]+\.[a-zA-Z]{2,}(\/\S*)?$/;
+  if (!urlPattern.test(originalUrl)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Store the URL in the database
+  const shortUrl = id++;
+  urlDatabase.push({ original_url: originalUrl, short_url: shortUrl });
+
+  // Respond with the original and short URLs
+  res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// Endpoint to handle URL redirection
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  const { shortUrl } = req.params;
+
+  // Find the corresponding original URL
+  const urlEntry = urlDatabase.find(entry => entry.short_url == shortUrl);
+
+  if (!urlEntry) {
+    return res.status(404).json({ error: 'short url not found' });
+  }
+
+  // Redirect to the original URL
+  res.redirect(urlEntry.original_url);
 });
 
+// Set up the server
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
